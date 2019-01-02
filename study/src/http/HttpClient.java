010a05b84d53b8dc1a4193dcc98c0f2ca3b4dabf
이창준 <@LAPTOP-QTP4K312>
2019-01-02 15:45:33 +0900

    TEST_HTTP

---
+++ study/src/http/HttpClient.java
@@ -0,0 +1,35 @@
+package http;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class HttpClient {
+
+	public static void main(String[] args) throws IOException {
+		
+		Socket socket = new Socket();
+        socket.connect(new InetSocketAddress("www.google.com", 80));
+ 
+        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+        Scanner scanner = new Scanner(socket.getInputStream());
+ 
+        writer.println("GET / HTTP/1.1");
+        writer.println("Host: www.google.com");
+        writer.println();
+ 
+        writer.flush();
+ 
+        while(scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+ 
+        scanner.close();
+        writer.close();
+        socket.close();
+
+	}
+
+}
---
+++ study/src/http/HttpServer.java
@@ -0,0 +1,39 @@
+package http;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class HttpServer {
+
+	public static void main(String[] args) throws IOException {
+		
+		ServerSocket server = new ServerSocket(9999);
+		Socket socket =  server.accept();
+        Scanner scanner = new Scanner(socket.getInputStream());
+        PrintWriter printer = new PrintWriter(socket.getOutputStream());
+ 
+        while(scanner.hasNextLine()) {
+            String result = scanner.nextLine();
+            System.out.println(result);
+            if(result.length() == 0) {
+                break;
+            }
+        }
+ 
+        String result = "Success Message";
+ 
+        printer.println("HTTP/1.1 200 OK");
+        printer.println("Content-Type: text/html");
+        printer.println("Content-Length: " + result.length());
+        printer.println();
+        printer.println(result);
+        printer.flush();
+  
+        printer.close();
+        scanner.close();
+	}
+
+}
